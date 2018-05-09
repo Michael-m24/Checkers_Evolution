3c540ab4b71c8e62143fff1f1a7cd1fbd8423f28
etial1 <eti.alkaliy@gmail.com>
2018-05-09 11:15:41 +0300

    tree- beginning

---
+++ checkers/Tree.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace checkers
+{
+    class Tree
+    {
+        //The internal nodes
+        string[] operations = { "+", "-", "*", "/", "sin", "cos" };
+        //The Variables- Only Leaves
+        string[] eval_funcs = { "f1", "f2", "f3", "f4", "f5", "f6" };
+
+    }
+}
