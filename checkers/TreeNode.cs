f919ff1b6ff9cc1afeecfc0c9718955058444c96
etial1 <eti.alkaliy@gmail.com>
2018-05-17 11:08:55 +0300

    binary expretion tree

---
+++ checkers/TreeNode.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace checkers
+{
+    class TreeNode
+    {
+        bool is_leaf;
+        public string data;
+        public float value;
+        public TreeNode right, left, father;
+        public TreeNode()
+        {
+            is_leaf = true;
+            right = null;
+            left = null;
+            father = null;
+        }
+
+        public TreeNode(string data, bool is_leaf, TreeNode right, TreeNode left, TreeNode father)
+        {
+            this.data = data;
+            this.is_leaf = is_leaf;
+            this.right = right;
+            this.left = left;
+            this.father = father;
+            this.value = 0;
+        }
+
+        public TreeNode get_father() { return this.father; }
+        public void set_right_son(TreeNode son) { this.right = son; }
+        public void set_left_son(TreeNode son) { this.left = son; }
+        public void set_is_leaf(bool is_leaf) { this.is_leaf = is_leaf; }
+
+    }
+
+}
