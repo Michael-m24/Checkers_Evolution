a958191d92a4deab097a6d4caac77287948a3537
etial1 <eti.alkaliy@gmail.com>
2018-05-24 14:09:57 +0300

    trinary tree

--- checkers/TreeNode.cs
+++ checkers/TreeNode.cs
@@ -10,10 +10,12 @@ namespace checkers
     {
         bool is_leaf;
         public string data;
-        public float value;
-        public TreeNode right, left,middle, father;
+        public double value;
+        public TreeNode right, middle, left, father;
+        bool is_eval_func;
         public TreeNode()
         {
+            is_eval_func = false;
             is_leaf = true;
             right = null;
             middle = null;
@@ -30,6 +32,7 @@ namespace checkers
             this.left = left;
             this.father = father;
             this.value = 0;
+            this.is_eval_func = false;
         }
 
         public TreeNode get_father() { return this.father; }
@@ -37,6 +40,9 @@ namespace checkers
         public void set_middle_son(TreeNode son) { this.middle = son; }
         public void set_left_son(TreeNode son) { this.left = son; }
         public void set_is_leaf(bool is_leaf) { this.is_leaf = is_leaf; }
+        public void set_data(string op) { this.data = op; }
+        public bool get_is_eval_func() { return this.is_eval_func; }
+        public void set_is_eval_func(bool is_eval_func) { this.is_eval_func = is_eval_func; }
 
     }
 
