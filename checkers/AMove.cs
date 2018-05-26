34d71070be9cc4fd0eb88c6955f2dd1512b08139
Michael-m24 <misha.m249@gmail.com>
2018-05-26 13:34:48 +0300

    ign 3

--- checkers/AMove.cs
+++ checkers/AMove.cs
@@ -8,19 +8,64 @@ namespace checkers
 {
     class AMove
     {
-        public int[] From = new int[2];
-        public int[] To = new int[2];
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
 
         public AMove(int f1, int f2, int t1, int t2)
         {
-            From = new int[] { f1, f2 };
-            To = new int[] { t1, t2 };
+=======
+        public int[] From =new int[2];
+        public int[] To =new int[2];
+
+        public AMove(int f1, int f2, int t1, int t2)
+        {
+            From = new int[] {f1, f2};
+            To = new int[] {t1,t2};
+>>>>>>> parent of c3bad96... merge v2
+=======
+        public int[] From =new int[2];
+        public int[] To =new int[2];
+
+        public AMove(int f1, int f2, int t1, int t2)
+        {
+            From = new int[] {f1, f2};
+            To = new int[] {t1,t2};
+>>>>>>> parent of c3bad96... merge v2
+=======
+        public int[] From =new int[2];
+        public int[] To =new int[2];
+
+        public AMove(int f1, int f2, int t1, int t2)
+        {
+            From = new int[] {f1, f2};
+            To = new int[] {t1,t2};
+>>>>>>> parent of c3bad96... merge v2
         }
 
-        public void printAMove()
+        public void printAMove(AMove a)
         {
-            Console.WriteLine("(" + From[0] + "," + From[1] + ") => (" + To[0] + "," + To[1] + ")");
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
         }
     }
-
+=======
+            Console.WriteLine("(" + a.From[0] + "," + a.From[1] + ") => (" + a.To[0] + "," + a.To[1] + ")");
+        }
+    }
+      
+>>>>>>> parent of c3bad96... merge v2
+=======
+            Console.WriteLine("(" + a.From[0] + "," + a.From[1] + ") => (" + a.To[0] + "," + a.To[1] + ")");
+        }
+    }
+      
+>>>>>>> parent of c3bad96... merge v2
+=======
+            Console.WriteLine("(" + a.From[0] + "," + a.From[1] + ") => (" + a.To[0] + "," + a.To[1] + ")");
+        }
+    }
+      
+>>>>>>> parent of c3bad96... merge v2
 }
