5fae2add4a02bcc97b4cdb7a655ff16cec00b2f7
Michael-m24 <misha.m249@gmail.com>
2018-05-26 13:55:23 +0300

    amove fix

--- checkers/AMove.cs
+++ checkers/AMove.cs
@@ -8,64 +8,29 @@ namespace checkers
 {
     class AMove
     {
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
+
+
+
+
+        public int[] From = new int[2];
+        public int[] To = new int[2];
 
         public AMove(int f1, int f2, int t1, int t2)
         {
-=======
-        public int[] From =new int[2];
-        public int[] To =new int[2];
+            From = new int[] { f1, f2 };
+            To = new int[] { t1, t2 };
 
-        public AMove(int f1, int f2, int t1, int t2)
-        {
-            From = new int[] {f1, f2};
-            To = new int[] {t1,t2};
->>>>>>> parent of c3bad96... merge v2
-=======
-        public int[] From =new int[2];
-        public int[] To =new int[2];
 
-        public AMove(int f1, int f2, int t1, int t2)
-        {
-            From = new int[] {f1, f2};
-            To = new int[] {t1,t2};
->>>>>>> parent of c3bad96... merge v2
-=======
-        public int[] From =new int[2];
-        public int[] To =new int[2];
 
-        public AMove(int f1, int f2, int t1, int t2)
-        {
-            From = new int[] {f1, f2};
-            To = new int[] {t1,t2};
->>>>>>> parent of c3bad96... merge v2
         }
+
 
         public void printAMove(AMove a)
         {
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-        }
-    }
-=======
             Console.WriteLine("(" + a.From[0] + "," + a.From[1] + ") => (" + a.To[0] + "," + a.To[1] + ")");
+
         }
+
     }
-      
->>>>>>> parent of c3bad96... merge v2
-=======
-            Console.WriteLine("(" + a.From[0] + "," + a.From[1] + ") => (" + a.To[0] + "," + a.To[1] + ")");
-        }
-    }
-      
->>>>>>> parent of c3bad96... merge v2
-=======
-            Console.WriteLine("(" + a.From[0] + "," + a.From[1] + ") => (" + a.To[0] + "," + a.To[1] + ")");
-        }
-    }
-      
->>>>>>> parent of c3bad96... merge v2
-}
+}   
+
