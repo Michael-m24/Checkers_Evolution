7ce61f651d95f336ef2ff5f1598dcea2f782174f
flAdar <35167996+flAdar@users.noreply.github.com>
2018-05-26 13:15:46 +0300

    Revert "merge v2"
    This reverts commit c3bad96be25e34577abacd2c1413a8b8cc61f6f5.

--- checkers/AMove.cs
+++ checkers/AMove.cs
@@ -9,9 +9,19 @@ namespace checkers
     class AMove
     {
 <<<<<<< HEAD
+<<<<<<< HEAD
 
         public AMove(int f1, int f2, int t1, int t2)
         {
+=======
+        public int[] From =new int[2];
+        public int[] To =new int[2];
+
+        public AMove(int f1, int f2, int t1, int t2)
+        {
+            From = new int[] {f1, f2};
+            To = new int[] {t1,t2};
+>>>>>>> parent of c3bad96... merge v2
 =======
         public int[] From =new int[2];
         public int[] To =new int[2];
@@ -26,8 +36,15 @@ namespace checkers
         public void printAMove(AMove a)
         {
 <<<<<<< HEAD
+<<<<<<< HEAD
         }
     }
+=======
+            Console.WriteLine("(" + a.From[0] + "," + a.From[1] + ") => (" + a.To[0] + "," + a.To[1] + ")");
+        }
+    }
+      
+>>>>>>> parent of c3bad96... merge v2
 =======
             Console.WriteLine("(" + a.From[0] + "," + a.From[1] + ") => (" + a.To[0] + "," + a.To[1] + ")");
         }
