50d51915fa031cfec6a497ef0d0d1e4269573250
flAdar <35167996+flAdar@users.noreply.github.com>
2018-05-26 13:35:12 +0300

    V6
    This reverts commit c3bad96be25e34577abacd2c1413a8b8cc61f6f5.

--- checkers/AMove.cs
+++ checkers/AMove.cs
@@ -8,64 +8,19 @@ namespace checkers
 {
     class AMove
     {
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
+        public int[] From = new int[2];
+        public int[] To = new int[2];
 
         public AMove(int f1, int f2, int t1, int t2)
         {
-=======
-        public int[] From =new int[2];
-        public int[] To =new int[2];
-
-        public AMove(int f1, int f2, int t1, int t2)
-        {
-            From = new int[] {f1, f2};
-            To = new int[] {t1,t2};
->>>>>>> parent of c3bad96... merge v2
-=======
-        public int[] From =new int[2];
-        public int[] To =new int[2];
-
-        public AMove(int f1, int f2, int t1, int t2)
-        {
-            From = new int[] {f1, f2};
-            To = new int[] {t1,t2};
->>>>>>> parent of c3bad96... merge v2
-=======
-        public int[] From =new int[2];
-        public int[] To =new int[2];
-
-        public AMove(int f1, int f2, int t1, int t2)
-        {
-            From = new int[] {f1, f2};
-            To = new int[] {t1,t2};
->>>>>>> parent of c3bad96... merge v2
+            From = new int[] { f1, f2 };
+            To = new int[] { t1, t2 };
         }
 
-        public void printAMove(AMove a)
+        public void printAMove()
         {
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
+            Console.WriteLine("(" + From[0] + "," + From[1] + ") => (" + To[0] + "," + To[1] + ")");
         }
     }
-=======
-            Console.WriteLine("(" + a.From[0] + "," + a.From[1] + ") => (" + a.To[0] + "," + a.To[1] + ")");
-        }
-    }
-      
->>>>>>> parent of c3bad96... merge v2
-=======
-            Console.WriteLine("(" + a.From[0] + "," + a.From[1] + ") => (" + a.To[0] + "," + a.To[1] + ")");
-        }
-    }
-      
->>>>>>> parent of c3bad96... merge v2
-=======
-            Console.WriteLine("(" + a.From[0] + "," + a.From[1] + ") => (" + a.To[0] + "," + a.To[1] + ")");
-        }
-    }
-      
->>>>>>> parent of c3bad96... merge v2
+
 }
